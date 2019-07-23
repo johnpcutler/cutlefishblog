bd23587f304c733a2bab3a88e537f6b7224489a0
John Cutler <johnpcutler@gmail.com>
2019-07-23 15:55:20 -0700

    changed some names

--- gatsby-node.js
+++ gatsby-node.js
@@ -10,8 +10,6 @@ const path = require(`path`)
 exports.createPages = ({ actions, graphql }) => {
   const { createPage } = actions
   const blogPostTemplate = path.resolve(`src/templates/blogTemplate.js`)
-  console.log("The path to the template is:")
-  console.log(blogPostTemplate)
   return graphql(`
     {
       allMarkdownRemark(
