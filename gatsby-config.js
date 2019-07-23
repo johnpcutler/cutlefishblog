b997f684724c3aa53bc20faaf2cec9967c31a329
John Cutler <johnpcutler@gmail.com>
2019-07-23 14:13:42 -0700

    adjusted title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `John Cutler's Blog`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
