7429e696f5cb75f7c87e595ced9e672b44f716d7
John Cutler <johnpcutler@gmail.com>
2019-07-24 09:50:35 -0700

    Working on site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `@johncutlefish`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `@johncutlefish's blog`,
+    description: `Writing on all things product by John Cutler (@johncutlefish).`,
+    author: `John Cutler`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
